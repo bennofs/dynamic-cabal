101338ca0a2b1269bae41f3c6ada6813a6b51b39
jpmoresmau <jp@moresmau.fr>
2013-12-28 17:27:17 +0100

    add extra information needed from BuildWrapper. Bump version

--- src/Distribution/Client/Dynamic/PackageDescription.hs
+++ src/Distribution/Client/Dynamic/PackageDescription.hs
@@ -23,12 +23,17 @@ data BuildInfo
 data CompilerFlavor
 data Extension
 data Dependency
+data ModuleName
 instance Eq CompilerFlavor where _ == _ = undefined
 
 -- | The name of a target. Libraries don't have a name, they are always named after the package.
-data TargetName = Library | Executable String | TestSuite String | BenchSuite String deriving (Show, Eq, Read, Ord)
+data TargetName = Library [String] -- ^ contains the names of exposed modules
+  | Executable String FilePath -- ^ the path to the Main module
+  | TestSuite String (Maybe FilePath) -- ^ the path to the Main module, for stdio tests
+  | BenchSuite String 
+  deriving (Show, Eq, Read, Ord)
 
--- | A target is a single Library, an Excutable, a TestSuite or a Benchmark.
+-- | A target is a single Library, an Executable, a TestSuite or a Benchmark.
 data Target = Target
   { -- | The name of the target
     name         :: TargetName
@@ -46,6 +51,9 @@ data Target = Target
     -- | Additional options to pass to GHC when compiling source files.
   , ghcOptions   :: [String]
 
+    -- | Additional options to pass to CPP preprocessor when compiling source files.
+  , cppOptions   :: [String]
+
     -- | The extensions to enable/disable. The elements are like GHC's -X flags, a disabled extension 
     -- is represented as the extension name prefixed by 'No'.
     -- Example value: extensions = ["ScopedTypeVariables", "NoMultiParamTypeClasses"]
@@ -54,8 +62,13 @@ data Target = Target
     -- | The 'buildable' field in the package description.
   , buildable    :: Bool
 
+    -- | other modules included in the target
+  , otherModules :: [String]
+
     -- | Whether this target was enabled or not. This only matters for Benchmarks or Tests, Executables and Libraries are always enabled.
   , enabled      :: Bool
+  
+
   } deriving (Show, Eq, Read)
 
 buildable' :: Selector BuildInfo Bool
@@ -71,22 +84,39 @@ includeDirs' = selector $ const $ useValue "Distribution.PackageDescription" $ I
 -- | Get the names of the extensions to enable/disable for all source files in the package. If an extension should
 -- be disabled, it's name is prefixed by 'No'. This corresponds to the names of -X flags to pass to GHC.
 extensions' :: Selector BuildInfo [String]
-extensions' = selector $ const $ expr $ \bi -> applyE2 map' display' $ append' <>$ applyE defaultExtensions' bi <>$ applyE oldExtensions' bi
+extensions' = selector $ const $ expr $ \bi -> applyE2 map' display' $ applyE concat' $ expr $ map (<>$ bi) [defaultExtensions', oldExtensions', otherExtensions']
   where display' :: ExpG (Extension -> String)
         display' = useValue "Distribution.Text" $ Ident "display"
         
-        defaultExtensions', oldExtensions' :: ExpG (BuildInfo -> [Extension])
+        defaultExtensions', oldExtensions',otherExtensions' :: ExpG (BuildInfo -> [Extension])
         defaultExtensions' = useValue "Distribution.PackageDescription" $ Ident "defaultExtensions"
         oldExtensions' = useValue "Distribution.PackageDescription" $ Ident "oldExtensions"
+        otherExtensions' = useValue "Distribution.PackageDescription" $ Ident "otherExtensions"
 
 -- | Get the options to pass to GHC for a given BuildInfo.
-ghcOptions' :: Selector BuildInfo [FilePath]
+ghcOptions' :: Selector BuildInfo [String]
 ghcOptions' = selector $ const $ concat' <>. applyE map' snd' <>. applyE filter' (applyE equal' ghc <>. fst') <>. options'
   where options' :: ExpG (BuildInfo -> [(CompilerFlavor, [String])])
         options' = useValue "Distribution.PackageDescription" $ Ident "options"
 
         ghc :: ExpG CompilerFlavor
         ghc = useValue "Distribution.Compiler" $ Ident "GHC"
+
+-- | Get the options to pass to GHC for a given BuildInfo.
+cppOptions' :: Selector BuildInfo [String]
+cppOptions' = selector $ const options'
+  where options' :: ExpG (BuildInfo -> [String])
+        options' = useValue "Distribution.PackageDescription" $ Ident "cppOptions"
+
+-- | Get the options to pass to GHC for a given BuildInfo.
+otherModules' :: Selector BuildInfo [String]
+otherModules' = selector $ const $ applyE map' display'  <>. mods'
+  where 
+        display' :: ExpG (Distribution.Client.Dynamic.PackageDescription.ModuleName -> String)
+        display' = useValue "Distribution.Text" $ Ident "display"
+        mods' :: ExpG (BuildInfo -> [Distribution.Client.Dynamic.PackageDescription.ModuleName])
+        mods' = useValue "Distribution.PackageDescription" $ Ident "otherModules"
+
 
 -- | Get the dependencies of the target and the version of the dependency if possible. If the dependencies version
 -- is not a specific version (this only happens when the target is not enabled), return Nothing.
@@ -110,37 +140,54 @@ dependencies' = selector $ const $ applyE map' serializeDep <>. targetBuildDepen
 
 -- | Construct a 'Target' from a buildInfo, a targetName and a Bool that is True if the target is enabled, false otherwise.
 buildInfoTarget :: Query BuildInfo (TargetName -> Bool -> Target)
-buildInfoTarget = (\d src inc opts exts ba n -> Target n d src inc opts exts ba)
+buildInfoTarget = (\d src inc opts copts exts ba oths n-> Target n d src inc opts copts exts ba oths)
                  <$> query dependencies' 
                  <*> query hsSourceDirs' 
                  <*> query includeDirs' 
                  <*> query ghcOptions'
+                 <*> query cppOptions'
                  <*> query extensions'
                  <*> query buildable'
+                 <*> query otherModules'
 
--- | Get the buildInfo of the library in the package. If there is no library in the package, 
+-- | Get the buildInfo of the library in the package, and its exposed mdoules. If there is no library in the package, 
 -- return the empty list.
-library' :: ExpG (PackageDescription -> [BuildInfo])
+library' :: ExpG (PackageDescription -> [([String],BuildInfo)])
 library' = applyE2 maybe' (returnE $ List []) serialize' <>. useValue "Distribution.PackageDescription" (Ident "library")
-  where serialize' = expr $ \lib -> expr [applyE buildInfo' lib]
+  where serialize' = expr $ \lib ->applyE2 cons (tuple2 <>$ applyE modNames' lib <>$ applyE buildInfo' lib) (returnE $ List [])
+        modNames'=applyE map' display'  <>. mods'
+        display' = useValue "Distribution.Text" $ Ident "display"
+        mods'   = useValue "Distribution.PackageDescription" $ Ident "exposedModules"
         buildInfo' = useValue "Distribution.PackageDescription" $ Ident "libBuildInfo"
 
--- | Get the buildInfo and the name of each executable in the package.
-executables' :: ExpG (PackageDescription -> [(String, BuildInfo)])
+-- | Get the buildInfo, the name and Main module path of each executable in the package.
+executables' :: ExpG (PackageDescription -> [((String,FilePath), BuildInfo)])
 executables'= applyE map' serialize' <>. useValue "Distribution.PackageDescription" (Ident "executables")
-  where serialize' = expr $ \exe -> tuple2 <>$ applyE exeName' exe <>$ applyE buildInfo' exe
+  where serialize' = expr $ \exe -> tuple2 <>$ applyE exeInfo exe <>$ applyE buildInfo' exe
+        exeInfo= expr $ \exe ->tuple2 <>$ applyE exeName' exe <>$ applyE modulePath' exe 
         exeName'   = useValue "Distribution.PackageDescription" $ Ident "exeName"
+        modulePath'= useValue "Distribution.PackageDescription" $ Ident "modulePath"
         buildInfo' = useValue "Distribution.PackageDescription" $ Ident "buildInfo"
 
--- | Get the name, whether the target is enabled or not and the buildInfo of each testSuite in the package.
-tests' :: ExpG (PackageDescription -> [((String, Bool), BuildInfo)])
+
+-- | Get the name, whether the target is enabled or not, possibly the main module path and the buildInfo of each testSuite in the package.
+tests' :: ExpG (PackageDescription -> [((String, Bool,Maybe FilePath), BuildInfo)])
 tests' = applyE map' serialize' <>. useValue "Distribution.PackageDescription" (Ident "testSuites")
   where serialize' = expr $ \test -> tuple2 
-                                    <>$ applyE2 tuple2 (testName' <>$ test) (testEnabled' <>$ test) 
+                                    <>$ applyE3 tuple3 (testName' <>$ test) (testEnabled' <>$ test) (testPath <>. testInterface' <>$ test) 
                                     <>$ applyE buildInfo' test
         testName'   = useValue "Distribution.PackageDescription" $ Ident "testName"
         testEnabled' = useValue "Distribution.PackageDescription" $ Ident "testEnabled"
         buildInfo' = useValue "Distribution.PackageDescription" $ Ident "testBuildInfo"
+        testInterface' = useValue "Distribution.PackageDescription" $ Ident "testInterface"
+        testPath=expr $ \ti->do
+                v10  <- useCon "Distribution.PackageDescription" $ Ident "TestSuiteExeV10"
+                versionVar  <- newName "version"
+                fpVar  <- newName "filepath"                
+                caseE ti 
+                        [(PApp v10 [PVar versionVar,PVar fpVar],applyE just' (useVar fpVar))
+                        ,(PWildCard,nothing')
+                        ] 
 
 -- | Get the name, whether it's enabled or not and the buildInfo of each benchmark in the package.
 benchmarks' :: ExpG (PackageDescription -> [((String, Bool), BuildInfo)])
@@ -156,23 +203,23 @@ benchmarks' = applyE map' serialize' <>. useValue "Distribution.PackageDescripti
 -- The resulting list is in the same order and has the same length as the list returned
 -- by buildInfos.
 targetInfos :: Query PackageDescription [(TargetName, Bool)]
-targetInfos = build <$> query hasLib <*> query exeNames <*> query testInfo <*> query benchInfo
-  where hasLib :: Selector PackageDescription Bool
-        hasLib = selector $ const $ not' <>. null' <>. library'
+targetInfos = build <$> query libMods <*> query exeNames <*> query testInfo <*> query benchInfo
+  where libMods :: Selector PackageDescription [[String]]
+        libMods =  selector $ const $ applyE map' fst' <>. library'
 
-        exeNames :: Selector PackageDescription [String]
+        exeNames :: Selector PackageDescription [(String,FilePath)]
         exeNames = selector $ const $ applyE map' fst' <>. executables'
 
-        testInfo :: Selector PackageDescription [(String, Bool)]
+        testInfo :: Selector PackageDescription [(String, Bool,Maybe FilePath)]
         testInfo = selector $ const $ applyE map' fst' <>. tests'
 
         benchInfo :: Selector PackageDescription [(String, Bool)]
         benchInfo = selector $ const $ applyE map' fst' <>. benchmarks'
 
         build lib exe test bench = concat
-          [ [ (Library      , True) | lib           ]
-          , [ (Executable x , True) | x <- exe       ]
-          , [ (TestSuite  x , e)    | (x,e) <- test  ]
+          [ [ (Library    x , True) | x <- lib           ]
+          , [ (Executable x mp, True) | (x,mp) <- exe       ]
+          , [ (TestSuite  x mp, e)    | (x,e,mp) <- test  ]
           , [ (BenchSuite x , e)    | (x,e) <- bench ]
           ]
 
@@ -180,7 +227,7 @@ targetInfos = build <$> query hasLib <*> query exeNames <*> query testInfo <*> q
 buildInfos :: Selector PackageDescription [BuildInfo]
 buildInfos = selector $ const $ expr $ \bi -> applyE concat' $ expr $ map (<>$ bi) [libraryBI, exesBI, testsBI, benchsBI]
   where libraryBI :: ExpG (PackageDescription -> [BuildInfo])
-        libraryBI = library'
+        libraryBI = applyE map' snd' <>. library'
         
         exesBI    :: ExpG (PackageDescription -> [BuildInfo])
         exesBI    = applyE map' snd' <>. executables'
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,14 +1,19 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, ScopedTypeVariables #-}
 import Data.List
 import Distribution.Client.Dynamic
 import Test.HUnit
 import Test.Tasty.HUnit
 import Test.Tasty.TH
+import qualified Data.Map as DM
+import           System.Directory
 
 case_targets :: Assertion
 case_targets = do
   tgs <- runQuery (on localPkgDesc targets) "dist/setup-config"
-  assertEqual "target names" (sort [Library, TestSuite "dynamic-cabal-tests", TestSuite "doctests"]) (sort $ map name tgs) 
+  assertEqual "target names" (sort [Library ["Distribution.Client.Dynamic"
+      ,"Distribution.Client.Dynamic.Query"
+      ,"Distribution.Client.Dynamic.LocalBuildInfo"
+      ,"Distribution.Client.Dynamic.PackageDescription"], TestSuite "dynamic-cabal-tests" (Just "Main.hs"), TestSuite "doctests" (Just "doctests.hs")]) (sort $ map name tgs) 
   assertEqual "source directories" (sort $ map sourceDirs tgs) $ sort $ map return ["src", "tests", "tests"]
   assertBool "ghc options" $ all (elem "-Wall" . ghcOptions)  tgs
   assertBool "no extensions" $ all (null . extensions) tgs
@@ -18,6 +23,48 @@ case_packageDBs :: Assertion
 case_packageDBs = do
   dbs <- runQuery packageDBs "dist/setup-config"
   length dbs `seq` return ()
+
+-- | test running raw query
+case_raw :: Assertion
+case_raw = do
+  s<-src
+  m::DM.Map String [String]<-runRawQuery s "dist/setup-config"
+  assertEqual "map size" 3 (DM.size m)
+  -- print m
+
+-- | the raw query source code
+src :: IO String
+src=do
+  setupConfig' <- canonicalizePath "dist/setup-config"
+  return $ unlines [
+    "module DynamicCabalQuery where"
+    ,"import Distribution.PackageDescription"
+    ,"import Distribution.Simple.LocalBuildInfo"
+    ,"import Data.IORef"
+    ,"import qualified Data.Map as DM"
+    ,"import qualified Distribution.Verbosity as V"
+    ,"import Data.Version (parseVersion)"
+    ,"import Text.ParserCombinators.ReadP(readP_to_S)"
+    ,"import Distribution.Simple.Program.GHC"
+    ,"import Distribution.Simple.GHC"
+    ,"import Distribution.Version"
+    ,"import Control.Monad"
+    ,""
+    ,"result :: IO (DM.Map String [String])"
+    ,"result=do"
+    ,"lbi<-liftM (read . Prelude.unlines . drop 1 . lines) $ Prelude.readFile \""++setupConfig' ++"\""
+    ,"let pkg=localPkgDescr lbi"
+    ,"r<-newIORef DM.empty"
+    ,"withComponentsLBI pkg lbi (\\c clbi->do"
+    ,"       let b=componentBuildInfo c"
+    ,"       let opts=renderGhcOptions ((fst $ head $ readP_to_S  parseVersion  \"7.6.3\") :: Version) $ componentGhcOptions V.silent lbi b clbi \"dist/build\"" 
+    ,"       let n=foldComponent (const \"\") exeName testName benchmarkName c"
+    ,"       modifyIORef r (DM.insert n opts)"
+    ,"       return ()"
+    ,"       )"
+    ,"readIORef r" ]
+
+
 
 main :: IO ()
 main = $(defaultMainGenerator)
